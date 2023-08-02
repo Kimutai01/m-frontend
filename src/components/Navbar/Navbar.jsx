938c9c80fe6617c78587ef3f0ad16372414f8d38
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-08-02 13:17:35 +0300

    Profile page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { toast } from 'react-toastify';
-import { AiOutlineClose } from 'react-icons/ai';
-import { CiUser } from 'react-icons/ci';
-import { useNavigate, Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { AiOutlineClose } from "react-icons/ai";
+import { CiUser } from "react-icons/ci";
+import { useNavigate, Link } from "react-router-dom";
 
-import { RiMenu5Line } from 'react-icons/ri';
-import { HiShoppingCart } from 'react-icons/hi';
-import './Navbar.css';
-import { BsChevronDown } from 'react-icons/bs';
+import { RiMenu5Line } from "react-icons/ri";
+import { HiShoppingCart } from "react-icons/hi";
+import "./Navbar.css";
+import { BsChevronDown } from "react-icons/bs";
 
-import logo from '../../assets/logo.png';
-import { logoutUser, selectUser } from '../../features/userSlice';
-import { selectCartItemsCount } from '../../features/cartSlice';
-import sportpesa from '../../assets/SP_logo-01.png';
-import fin from '../../assets/finix-casino.png';
+import logo from "../../assets/logo.png";
+import { logoutUser, selectUser } from "../../features/userSlice";
+import { selectCartItemsCount } from "../../features/cartSlice";
+import sportpesa from "../../assets/SP_logo-01.png";
+import fin from "../../assets/finix-casino.png";
 
 const NavBar = () => {
   const navigate = useNavigate();
   const user = useSelector(selectUser);
   const [nav, setNav] = useState(false);
   const [shadow, setShadow] = useState(false);
-  const [navBg, setNavBg] = useState('#ecf0f3');
-  const [linkColor, setLinkColor] = useState('#1f2937');
+  const [navBg, setNavBg] = useState("#ecf0f3");
+  const [linkColor, setLinkColor] = useState("#1f2937");
 
   useEffect(() => {
-    setNavBg('#000');
-    setLinkColor('#ecf0f3');
+    setNavBg("#000");
+    setLinkColor("#ecf0f3");
   }, []);
 
   useEffect(() => {
@@ -38,7 +38,7 @@ const NavBar = () => {
       }
     };
 
-    window.addEventListener('scroll', handleShadow);
+    window.addEventListener("scroll", handleShadow);
   }, []);
 
   const handleNav = () => {
@@ -57,14 +57,14 @@ const NavBar = () => {
   const logoutHandler = () => {
     dispatch(logoutUser());
 
-    toast.success('Logged out successfully', {
-      position: 'top-center',
+    toast.success("Logged out successfully", {
+      position: "top-center",
       autoClose: 5000,
       hideProgressBar: false,
       closeOnClick: true,
       draggable: true,
     });
-    navigate('/');
+    navigate("/");
   };
 
   return (
@@ -149,8 +149,8 @@ const NavBar = () => {
         style={{ backgroundColor: `${navBg}` }}
         className={
           shadow
-            ? 'w-full h-20 shadow-xl z-[100] md:px-24'
-            : 'w-full h-20 z-[100] md:px-24'
+            ? "w-full h-20 shadow-xl z-[100] md:px-24"
+            : "w-full h-20 z-[100] md:px-24"
         }
       >
         <div className="flex justify-between items-center w-full h-full py-4 px-2 rounded">
@@ -264,7 +264,7 @@ const NavBar = () => {
             <div className="flex">
               {user ? (
                 <div className="group ml-10">
-                  <li className="font-medium  text-xl text-[#f4e721] mt-[10px] hover:underline underline-offset-8 hover:scale-105 transition-all duration-all hover:border-y-black list-none">
+                  <li className="font-medium  text-xl text-[#f4e721] mt-[10px] hover:underline underline-offset-8 hover:scale-105 transition-all duration-all capitalize hover:border-y-black list-none">
                     {user.name}
                     <div className="opacity-0 group-hover:opacity-100 w-[200px] absolute left-0 top-full bg-[#000] pt-10 py-2 rounded-lg shadow-lg">
                       <ul className="p-2 flex flex-col">
@@ -313,15 +313,15 @@ const NavBar = () => {
         <div
           className={
             nav
-              ? 'md:hiddden fixed left-0 top-0 w-full h-screen bg-black/70'
-              : ''
+              ? "md:hiddden fixed left-0 top-0 w-full h-screen bg-black/70"
+              : ""
           }
         >
           <div
             className={
               nav
-                ? ' fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%]  h-screen bg-[#000] text-white p-2 ease-in duration-500'
-                : 'fixed left-[-100%] top-0  p-10 ease-in duration-500'
+                ? " fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%]  h-screen bg-[#000] text-white p-2 ease-in duration-500"
+                : "fixed left-[-100%] top-0  p-10 ease-in duration-500"
             }
           >
             <div>
@@ -353,7 +353,7 @@ const NavBar = () => {
                         onClick={handleServicesClick}
                       >
                         <div className="text-2xl uppercase text-[#f4e721] underline-offset-8 hover:text-[#f4e721]">
-                          {' '}
+                          {" "}
                           {user.name}
                         </div>
                         <div>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { ToastContainer, toast } from 'react-toastify';
-import { useLocation, useNavigate, Link } from 'react-router-dom';
-import { ImCross } from 'react-icons/im';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { ToastContainer, toast } from "react-toastify";
+import { useLocation, useNavigate, Link } from "react-router-dom";
+import { ImCross } from "react-icons/im";
 
 import {
   selectUserDetails,
   updateUserProfile,
   getUserDetails,
-} from '../features/profileSlice';
-import { selectUser } from '../features/userSlice';
-import { listAllOrders, selectOrders } from '../features/orderSlice';
+} from "../features/profileSlice";
+import { selectUser } from "../features/userSlice";
+import { listAllOrders, selectOrders } from "../features/orderSlice";
 
 const Profile = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [message, setMessage] = useState(null);
 
   const orders = useSelector(selectOrders);
@@ -24,12 +24,12 @@ const Profile = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
-  const redirect = location.search ? location.search.split('=')[1] : '/';
+  const redirect = location.search ? location.search.split("=")[1] : "/";
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
   const submitHandler = (e) => {
     if (password !== confirmPassword) {
-      setMessage('Passwords do not match');
+      setMessage("Passwords do not match");
     } else {
       e.preventDefault();
       dispatch(
@@ -38,12 +38,12 @@ const Profile = () => {
           name,
           email,
           password,
-        }),
+        })
       );
     }
   };
   toast.error(message, {
-    position: 'top-center',
+    position: "top-center",
     autoClose: 5000,
     hideProgressBar: false,
     closeOnClick: true,
@@ -54,9 +54,9 @@ const Profile = () => {
 
   useEffect(() => {
     if (!user) {
-      navigate('/login');
+      navigate("/login");
     } else if (!userDetails.name || user._id !== userDetails._id) {
-      dispatch(getUserDetails('profile'));
+      dispatch(getUserDetails("profile"));
     } else {
       setName(userDetails.name);
       setEmail(userDetails.email);
@@ -71,7 +71,7 @@ const Profile = () => {
     <>
       <ToastContainer />
       <div className="bg-[#000] flex flex-col md:flex-row pt-24 px-5 md:px-24 pb-20 gap-10">
-        <div className="bg-[#161616] mx-auto w-[30%] px-10 rounded-lg pb-10">
+        <div className="bg-[#161616] mx-auto md:w-[30%] px-10 rounded-lg pb-10">
           <div className="flex justify-center md:flex-row gap-5 pt-10">
             <div className="flex flex-col w-full">
               <label
@@ -79,6 +79,7 @@ const Profile = () => {
                 className="text-white mb-3 uppercase font-bold"
               >
                 Name
+                <br />
                 <input
                   type="text"
                   id="email"
@@ -87,7 +88,7 @@ const Profile = () => {
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                   placeholder="Your name.."
-                  className="bg-[#161616] text-white border-[grey] border-[1px] rounded-lg p-2 font-medium focus:outline-none focus:border-[#ff4d24]"
+                  className="bg-[#161616] mt-5 text-white w-full border-[grey] border-[1px] rounded-lg p-2 font-medium focus:outline-none focus:border-[#ff4d24]"
                 />
               </label>
             </div>
@@ -99,6 +100,7 @@ const Profile = () => {
                 className="text-white mb-3 uppercase font-bold"
               >
                 Email Address
+                <br />
                 <input
                   type="text"
                   required
@@ -107,7 +109,7 @@ const Profile = () => {
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   placeholder="Your email address.."
-                  className="bg-[#161616] text-white border-[grey] border-[1px] rounded-lg p-2 font-medium focus:outline-none focus:border-[#ff4d24]"
+                  className="bg-[#161616] mt-5 text-white w-full border-[grey] border-[1px] rounded-lg p-2 font-medium focus:outline-none focus:border-[#ff4d24]"
                 />
               </label>
             </div>
@@ -119,6 +121,7 @@ const Profile = () => {
                 className="text-white mb-3 uppercase font-bold"
               >
                 Password
+                <br />
                 <input
                   type="password"
                   id="password"
@@ -126,7 +129,7 @@ const Profile = () => {
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   placeholder="Your password.."
-                  className="bg-[#161616] text-white border-[grey] border-[1px] rounded-lg p-2 font-medium focus:outline-none focus:border-[#ff4d24]"
+                  className="bg-[#161616] mt-5 text-white w-full border-[grey] border-[1px] rounded-lg p-2 font-medium focus:outline-none focus:border-[#ff4d24]"
                 />
               </label>
             </div>
@@ -138,6 +141,7 @@ const Profile = () => {
                 className="text-white mb-3 uppercase font-bold"
               >
                 Password
+                <br />
                 <input
                   type="password"
                   id="confirmPassword"
@@ -145,7 +149,7 @@ const Profile = () => {
                   value={confirmPassword}
                   onChange={(e) => setConfirmPassword(e.target.value)}
                   placeholder="Confirm your password.."
-                  className="bg-[#161616] text-white border-[grey] border-[1px] rounded-lg p-2 font-medium focus:outline-none focus:border-[#ff4d24]"
+                  className="bg-[#161616] mt-5 w-full text-white border-[grey] border-[1px] rounded-lg p-2 font-medium focus:outline-none focus:border-[#ff4d24]"
                 />
               </label>
             </div>
@@ -158,85 +162,74 @@ const Profile = () => {
           >
             <h1 className="font-bold">Update profile</h1>
           </button>
-          <p className="text-[#fff] font-medium">
-            Already have an account?
-            {' '}
-            <Link
-              to={redirect ? `/login?redirect=${redirect}` : '/login'}
-              className="text-[#ff4d24]"
-            >
-              Sign In
-            </Link>
-          </p>
         </div>
         <div>
           <h1 className="text-white text-4xl font-bold uppercase text-center">
             My Orders
           </h1>
 
-          <table className="table-auto mt-10">
-            <thead>
-              <tr>
-                <th className="text-white text-lg font-bold uppercase px-4 py-2">
-                  Order ID
-                </th>
-                <th className="text-white text-lg font-bold uppercase px-4 py-2">
-                  Date
-                </th>
-                <th className="text-white text-lg font-bold uppercase px-4 py-2">
-                  Total
-                </th>
-                <th className="text-white text-lg font-bold uppercase px-4 py-2">
-                  Paid
-                </th>
-                <th className="text-white text-lg font-bold uppercase px-4 py-2">
-                  Delivered
-                </th>
-                <th className="text-white text-lg font-bold uppercase px-4 py-2">
-                  Details
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders.map((order) => (
-                <tr key={order._id} className="border-b-[1px] border-[#fae115]">
-                  <td className="text-white text-lg font-medium px-4 py-2">
-                    {order._id}
-                  </td>
-                  <td className="text-white text-lg font-medium px-4 py-2">
-                    {order.createdAt.substring(0, 10)}
-                  </td>
-                  <td className="text-white text-lg font-medium px-4 py-2">
-                    {order.totalPrice}
-                  </td>
-                  <td className="text-white text-lg font-medium px-4 py-2">
-                    {order.isPaid ? (
-                      order.paidAt.substring(0, 10)
-                    ) : (
-                      <ImCross className="text-[#ff4d24] ml-10 text-2xl" />
-                    )}
-                  </td>
-                  <td className="text-white text-lg font-medium px-4 py-2">
-                    {order.isDelivered ? (
-                      order.deliveredAt.substring(0, 10)
-                    ) : (
-                      <ImCross className="text-[#ff4d24]  text-2xl ml-10" />
-                    )}
-                  </td>
-                  <td className="text-[#000] text-lg font-medium px-4 py-2">
-                    <Link to={`/order/${order._id}`}>
-                      <button
-                        className="bg-[#fae115] text-[#000] px-4 py-2 rounded-lg"
-                        type="button"
-                      >
-                        <h1 className="font-bold">Details</h1>
-                      </button>
-                    </Link>
-                  </td>
+          <div className="overflow-x-auto">
+            <table className="table-auto mt-10">
+              <thead>
+                <tr>
+                  <th className="text-white text-lg font-bold uppercase px-4 py-2">
+                    Date
+                  </th>
+                  <th className="text-white text-lg font-bold uppercase px-4 py-2">
+                    Total
+                  </th>
+                  <th className="text-white text-lg font-bold uppercase px-4 py-2">
+                    Paid
+                  </th>
+                  <th className="text-white text-lg font-bold uppercase px-4 py-2">
+                    Delivered
+                  </th>
+                  <th className="text-white text-lg font-bold uppercase px-4 py-2">
+                    Details
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {orders.map((order) => (
+                  <tr
+                    key={order._id}
+                    className="border-b-[1px] border-[#fae115]"
+                  >
+                    <td className="text-white text-lg font-medium px-4 py-2">
+                      {order.createdAt.substring(0, 10)}
+                    </td>
+                    <td className="text-white text-lg font-medium px-4 py-2">
+                      {order.totalPrice}
+                    </td>
+                    <td className="text-white text-lg font-medium px-4 py-2">
+                      {order.isPaid ? (
+                        order.paidAt.substring(0, 10)
+                      ) : (
+                        <ImCross className="text-[#ff4d24] ml-10 text-2xl" />
+                      )}
+                    </td>
+                    <td className="text-white text-lg font-medium px-4 py-2">
+                      {order.isDelivered ? (
+                        order.deliveredAt.substring(0, 10)
+                      ) : (
+                        <ImCross className="text-[#ff4d24]  text-2xl ml-10" />
+                      )}
+                    </td>
+                    <td className="text-[#000] text-lg font-medium px-4 py-2">
+                      <Link to={`/order/${order._id}`}>
+                        <button
+                          className="bg-[#fae115] text-[#000] px-4 py-2 rounded-lg"
+                          type="button"
+                        >
+                          <h1 className="font-bold">Details</h1>
+                        </button>
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </>
