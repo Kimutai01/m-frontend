3df4acae544e323277512cd2b489d8c51dde9a8f
ger619 <draker74@gmail.com>
2023-08-13 23:43:45 +0300

    add changes to match fixtures

--- src/components/Matches/index.jsx
+++ src/components/Matches/index.jsx
@@ -1,13 +1,17 @@
 import SingleMatch from './SingleMatch';
 import Membership from './Membership';
+import Adverts from './Adverts';
 
 const Ticket = () => (
   <div>
     <h1 className="p-8 bg-[#FAE115] font-bold uppercase text-5xl">Tickets</h1>
-    <div className="m-[4%] ">
-      <SingleMatch />
-      <SingleMatch />
-      <SingleMatch />
+    <div className="flex flex-col justify-center md:flex-row gap-5   m-[4%]">
+      <div>
+        <SingleMatch />
+        <SingleMatch />
+        <SingleMatch />
+      </div>
+      <Adverts />
     </div>
     <Membership />
   </div>
